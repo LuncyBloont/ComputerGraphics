9dfb1452cb80fbbd8d7b12dae51a00f0589be2fa
LuncyBloont <xhymaker@126.com>
2021-12-01 08:49:11 +0800

    Cut off GLTools from Sample0

--- Samples/OpenGL0/triangle.cpp
+++ Samples/OpenGL0/triangle.cpp
@@ -2,15 +2,15 @@
 #include <GL/glut.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <GLTools.h>
 
-#define SPEED 0.001f;
-
-GLBatch triangle;
-GLShaderManager shaderManager;
+#define SPEED 0.01f;
+#define SPLIT4(p, k) p[0 + k * 4], p[1 + k * 4], p[2 + k * 4], p[3 + k * 4]
+#define SPLIT3(p, k) p[0 + k * 3], p[1 + k * 3], p[2 + k * 3]
 
 GLfloat movex = 0;
 GLfloat movey = 0;
+
+bool upToggle = false, downToggle = false, leftToggle = false, rightToggle = false;
 
 GLfloat vertexs[] = {
     -0.5f, 0.0f, 0.0f,
@@ -21,11 +21,8 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     GLfloat blue[] = {0.3f, 0.5f, 1.0f, 1.0f};
-    shaderManager.UseStockShader(GLT_SHADER_IDENTITY, blue);
-
-    triangle.Draw();
-
-    glutSwapBuffers();
+    GLfloat red[] = {0.9f, 0.5f, 0.2f, 1.0f};
+    GLfloat green[] = {0.3f, 1.0f, 0.2f, 1.0f};
 
     vertexs[0] += movex;
     vertexs[3] += movex;
@@ -34,18 +31,23 @@ void display()
     vertexs[4] += movey;
     vertexs[7] += movey;
 
-    triangle.CopyVertexData3f(vertexs);
+    glBegin(GL_TRIANGLES);
+    glColor4f(SPLIT4(blue, 0));
+    glVertex3f(SPLIT3(vertexs, 0));
+    glColor4f(SPLIT4(red, 0));
+    glVertex3f(SPLIT3(vertexs, 1));
+    glColor4f(SPLIT4(green, 0));
+    glVertex3f(SPLIT3(vertexs, 2));
+    glEnd();
+    glFlush();
+    glutSwapBuffers();
+
     glutPostRedisplay();
 }
 
 void setup()
 {
     glClearColor(0.3f, 0.2f, 0.1f, 1.0f);
-    shaderManager.InitializeStockShaders();
-
-    triangle.Begin(GL_TRIANGLES, 3);
-    triangle.CopyVertexData3f(vertexs);
-    triangle.End();
 }
 
 void resizeWindow(int w, int h)
@@ -58,15 +60,23 @@ void inputFunc(int key, int x, int y)
     switch (key)
     {
     case GLUT_KEY_LEFT:
+        if (leftToggle) break;
+        leftToggle = true;
         movex -= SPEED;
         break;
     case GLUT_KEY_RIGHT:
+        if (rightToggle) break;
+        rightToggle = true;
         movex += SPEED;
         break;
     case GLUT_KEY_UP:
+        if (upToggle) break;
+        upToggle = true;
         movey += SPEED;
         break;
     case GLUT_KEY_DOWN:
+        if (downToggle) break;
+        downToggle = true;
         movey -= SPEED;
         break;
     default:
@@ -80,15 +90,19 @@ void inputUpFunc(int key, int x, int y)
     {
     case GLUT_KEY_LEFT:
         movex += SPEED;
+        leftToggle = false;
         break;
     case GLUT_KEY_RIGHT:
         movex -= SPEED;
+        rightToggle = false;
         break;
     case GLUT_KEY_UP:
         movey -= SPEED;
+        upToggle = false;
         break;
     case GLUT_KEY_DOWN:
         movey += SPEED;
+        downToggle = false;
         break;
     default:
         break;
@@ -97,7 +111,6 @@ void inputUpFunc(int key, int x, int y)
 
 int main(int argc, char **argv)
 {
-    gltSetWorkingDirectory(argv[0]);
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
     glutInitWindowSize(800, 600);
